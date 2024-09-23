9cfc00dd2dfec4ba2eafc5f29dd5d82c5b396d1f
rverma6 <rayvaverma18@gmail.com>
2024-09-23 00:05:07 -0500

    favicon and app name fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="container">
       <main className="main-content">
         <div className="profile-section">
@@ -11,24 +13,26 @@ function App() {
             alt="Rayva Verma"
             className="profile-img"
           />
-          <h2 className="greeting-text">Hi, I'm Rayva Verma</h2>
+          <h2 className="greeting-text">Hi, I'm Rayva Verma.</h2>
         </div>
         <ul className="nav-links">
-          <li><a href="/About">About</a></li>
-          <li><a href="/Projects">Projects</a></li>
+        <li><Link to="/About">About</Link></li>
+        <li><a href="/Projects">Projects</a></li>
           <li><a href="/Talks">Resume</a></li>
-          <li><a href="/Blogs">Blogs</a></li>
-          <li><a href="https://github.com">GitHub</a></li>
+          <li><a href="https://substack.com/@rayvaverma" target="_blank" rel="noopener noreferrer">Blogs</a></li>
+          <li><a href="https://github.com/rverma6">GitHub</a></li>
         </ul>
         <div className="description">
           <p>
             I'm a senior at the University of Illinois at Urbana-Champaign studying computer science, statistics, and math.
-            I build stuff in my free time. Currently, I'm working on <a href="10xLabs.tech">10xLabs</a>, a student focused venture studio at the University of Illinois. 
-            I'm currently building Synergy, a platform to match job applicants with their perfect team fit. Feel free to reach out to me at <a href="rayvaverma@gmail.com">rayvaverma@gmail.com</a>.
+            I build stuff in my free time. Currently, I'm working on <a href="https://10xLabs.tech" target="_blank" rel="noopener noreferrer">10xLabs</a>
+            , a student focused venture studio at the University of Illinois. 
+            I'm also building Synergy, a platform to match job applicants with their perfect team fit. Feel free to reach out to me at <a href="rayvaverma@gmail.com">rayvaverma@gmail.com</a>.
           </p>
         </div>
       </main>
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+// pages/about.js
+
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is the About page content</p>
+        </div>
+    );
+};
+
+export default About;
